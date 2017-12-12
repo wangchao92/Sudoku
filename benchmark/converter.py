245f6f617ef4153733119aa91d4f59f70a4c9383
Wang Chao <chao.wang92@gmail.com>
2017-12-12 14:21:30 -0800

    Update benchmark

--- benchmark/converter.py
+++ benchmark/converter.py
@@ -6,12 +6,12 @@ def generate(fileName):
 				numbers = line.split()
 				print '{',
 				for num in numbers[:-1]:
-					if num == '*':
+					if num == '-1':
 						print '%4s' % '0,',
 					else:
 						print '%4s' % (str(int('0x' + num.lower(), 0) + 1) + ','),
 				
-				if numbers[-1] == '*':
+				if numbers[-1] == '-1':
 					print '%4s' % '0',
 				else:
 					print '%4s' % str(int('0x' + numbers[-1].lower(), 0) + 1),
@@ -19,4 +19,4 @@ def generate(fileName):
 		print '}'
 
 if __name__ == "__main__":
-	generate('hexa_expert.txt')
+	generate('hexa-20.txt')
